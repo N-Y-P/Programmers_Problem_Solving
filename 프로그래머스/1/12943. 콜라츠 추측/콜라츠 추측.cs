37b5c617aa5efc71aad378d9ec66e84d06b75aab
나연 <99782442+N-Y-P@users.noreply.github.com>
2025-09-03 21:58:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.28 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+
+        while(n != 1 && answer <500) {
+            n = (n % 2 == 0) ? (n / 2) : (3*n +1);
+            answer++;
+        }
+
+        return (n == 1) ? answer : -1;
+    }
+}

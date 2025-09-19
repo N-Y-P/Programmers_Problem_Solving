1c7243b7ac4d365de6e2466e5699e590e9bb98ec
나연 <99782442+N-Y-P@users.noreply.github.com>
2025-09-19 17:14:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 2.33 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string s) {
+        Dictionary<string, string> word = new Dictionary<string, string>() {
+            {"zero", "0"},
+            {"one", "1"},
+            {"two", "2"},
+            {"three", "3"},
+            {"four", "4"},
+            {"five", "5"},
+            {"six", "6"},
+            {"seven", "7"},
+            {"eight", "8"},
+            {"nine", "9"}
+        };
+        
+        foreach(var x in word) {
+            s = s.Replace(x.Key, x.Value);
+        }
+        
+        return int.Parse(s);
+    }
+}
